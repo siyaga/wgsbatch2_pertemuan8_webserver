696b3ae0ea25e2881fe9e281274a45109b32c09a
lifeforme <siyaga123@gmail.com>
2022-07-13 09:34:43 +0700

    membuat web server validasi dan membuat index html

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const http = require('http');   
+const port = 3000;
+
 http
     .createServer((req,res)=>{
-        res.write('hello world');
+        const url = req.url;
+        console.log(url);
+        // Menambahkan validasi untuk setiap pagenya
+        if(url==='/about'){
+            res.write('<h1>this is about page</h1>');
+            res.end();
+        }else  if(url==='/contact'){
+            res.write('<h1>this is contact page</h1>');
+            res.end();
+        }else {
+            res.write('hello world');
         res.end();
+        }
+        // res.writeHead(200, { 
+        //     'Content-Type': 'text/html' });
+        
     })
-    .listen(3000, ()=>{
+    // Memasukan Port yang akan di jalankan
+    .listen(port, ()=>{
         console.log('Server listening on port 3000');
     });
