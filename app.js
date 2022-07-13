9e88aa0684a5995e6227206047d92e94581c63e5
lifeforme <siyaga123@gmail.com>
2022-07-13 09:14:01 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');   
+http
+    .createServer((req,res)=>{
+        res.write('hello world');
+        res.end();
+    })
+    .listen(3000, ()=>{
+        console.log('Server listening on port 3000');
+    });
